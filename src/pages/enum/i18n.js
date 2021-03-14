992a0c05d5adb5f810a41f59097f8ae8b390ffc4
ConteMan <boxiaozhi.bolu@gmail.com>
2021-03-14 11:49:17 +0800

    :sparkles:  feat: 枚举管理, 增加排序以及扩展字段;

--- src/pages/enum/i18n.js
+++ src/pages/enum/i18n.js
@@ -5,11 +5,15 @@ module.exports = {
       form: {
         parent_id: '父节点',
         name: '名称',
+        column_name: '编码/名称',
         code: '编码',
         des: '描述',
         value_type: '值类型',
         value: '值',
         status: '状态',
+        sort: '排序',
+        sort_extra: '数字越大越靠前',
+        extend: '扩展',
       },
     },
     HK: {
@@ -17,11 +21,15 @@ module.exports = {
       form: {
         parent_id: '父节点',
         name: '名称',
+        column_name: '编码/名称',
         code: '编码',
         des: '描述',
         value_type: '值类型',
         value: '值',
         status: '状态',
+        sort: '排序',
+        sort_extra: '数字越大越靠前',
+        extend: '扩展',
       },
     },
     US: {
@@ -29,11 +37,15 @@ module.exports = {
       form: {
         parent_id: 'Parent Node',
         name: 'Name',
+        column_name: 'Code/Name',
         code: 'Code',
         des: 'Description',
         value_type: 'ValueType',
         value: 'Value',
         status: 'Status',
+        sort: 'sort',
+        sort_extra: 'bigger and the priority is higher',
+        extend: 'extend',
       },
     }
   }
